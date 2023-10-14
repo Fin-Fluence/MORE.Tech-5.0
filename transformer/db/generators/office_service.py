c8e54665ff1d0ac4acab491fd1130961b37cea79
George Pogrebnoi <gosha1416@yandex.ru>
2023-10-14 17:06:04 +0300

    Add office service generator

--- transformer/db/generators/office_service.py
+++ transformer/db/generators/office_service.py
@@ -6,9 +6,17 @@ class OfficeServiceGenerator:
   def __init__(self):
     self.model = OfficeService
     self.names = ['Test1', 'Test2']
+    self.bool_variants = [True, False]
 
   def generate_one(self, office):
-    pass
-    # os = OfficeService.create(
-    #   name=
-    # )
+    return 
+    os = OfficeService.create(
+      name=random.choice(self.names),
+      capability=random.choice(self.bool_variants),
+      activity=random.choice(self.bool_variants),
+      current_ticker=random.choice(self.bool_variants),
+      last_ticker=random.choice(self.bool_variants),
+      office=office
+    )
+
+    return os
